9bf984dca2b8241e0e85967c5063fc42aba3c764
UpcraftLP <j-schirra@gmx.de>
2018-05-10 17:30:15 +0200

    added empty inhaler recipe and recipe unlocking when smelting iron

--- src/main/java/com/github/upcraftlp/inhaler/GaspunkInhaler.java
+++ src/main/java/com/github/upcraftlp/inhaler/GaspunkInhaler.java
@@ -8,6 +8,7 @@ import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ModMetadata;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -62,7 +63,13 @@ public class GaspunkInhaler {
             UpdateChecker.registerMod(MODID);
             debugMode = ModHelper.isDebugMode();
         }
-        log.info("gaspunk inhaler initialized!");
+        if(debugMode) log.info("gaspunk inhaler has completed pre-initialization!");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        InhalerHandler.init();
+        if(debugMode) log.info("gaspunk inhaler has completed initialized!");
     }
 
 }
--- src/main/java/com/github/upcraftlp/inhaler/InhalerHandler.java
+++ src/main/java/com/github/upcraftlp/inhaler/InhalerHandler.java
@@ -2,16 +2,23 @@ package com.github.upcraftlp.inhaler;
 
 import com.github.upcraftlp.inhaler.api.IEntityGasEffect;
 import com.github.upcraftlp.inhaler.capability.CapabilityGasEffect;
+import com.google.common.collect.Lists;
 import ladysnake.gaspunk.api.IBreathingHandler;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.stats.RecipeBookServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod.EventBusSubscriber(modid = GaspunkInhaler.MODID)
 public class InhalerHandler {
@@ -31,4 +38,29 @@ public class InhalerHandler {
     public static void initCaps(AttachCapabilitiesEvent<Entity> event) {
         if(event.getObject() instanceof EntityPlayer) event.addCapability(new ResourceLocation(GaspunkInhaler.MODID, "gas_effect"), new CapabilityGasEffect());
     }
+
+    private static IRecipe EMPTY_INHALER_RECIPE, INHALER_RECIPE;
+    private static int OREDICT_ID_IRON_INGOT = -1;
+
+    @SubscribeEvent
+    public static void onCraftingIron(PlayerEvent.ItemSmeltedEvent event) {
+        if(event.player instanceof EntityPlayerMP) {
+            RecipeBookServer recipeBook = ((EntityPlayerMP) event.player).getRecipeBook();
+            if((recipeBook.isUnlocked(EMPTY_INHALER_RECIPE) && recipeBook.isUnlocked(INHALER_RECIPE))) return;
+            for(int id : OreDictionary.getOreIDs(event.smelting)) {
+                if(id == OREDICT_ID_IRON_INGOT) {
+                    event.player.unlockRecipes(Lists.newArrayList(EMPTY_INHALER_RECIPE, INHALER_RECIPE));
+                    if(GaspunkInhaler.isDebugMode()) GaspunkInhaler.getLogger().info("Player {} has unlocked the inhaler recipes!", event.player.getDisplayNameString());
+                }
+            }
+        }
+    }
+
+    static void init() {
+        OREDICT_ID_IRON_INGOT = OreDictionary.getOreID("ingotIron");
+        EMPTY_INHALER_RECIPE = CraftingManager.getRecipe(new ResourceLocation(GaspunkInhaler.MODID, "empty_inhaler"));
+        INHALER_RECIPE = CraftingManager.getRecipe(new ResourceLocation(GaspunkInhaler.MODID, "inhaler"));
+        if(GaspunkInhaler.isDebugMode()) GaspunkInhaler.getLogger().info("Ore Dictionary ID of \"ingotIron\": {}", OREDICT_ID_IRON_INGOT);
+    }
+
 }
